e04b7f37ca00362d96c2fa99f7a3f0b73500ecf9
eldinbautista7 <ebauti14@my.centennialcollege.ca>
2026-04-06 09:05:23 -0400

    feat: add POST /tasks endpoint - closes #2

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,12 +1,19 @@
 // src/routes/tasks.js
 const express = require('express');
 const router = express.Router();
-const { getAllTasks } = require('../data/store');
+const { getAllTasks, addTask } = require('../data/store');
 
 // GET /tasks - returns all tasks
 router.get('/', (req, res) => {
   const tasks = getAllTasks();
   res.status(200).json(tasks);
+});
+
+// POST /tasks
+router.post('/', (req, res) => {
+  const { title } = req.body;
+  const task = addTask(title);
+  res.status(200).json(task);
 });
 
 module.exports = router;
